ae1709762b00dded56e713dc20cbe2b5c22f7bad
lacanilaomarou <isiahmarou.lacanilao@benilde.edu.ph>
2020-07-10 19:16:37 +0800

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] 
+    private float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            transform.position += new Vector3(0, 1, 0) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
+            transform.position += new Vector3(0, -1, 0) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
